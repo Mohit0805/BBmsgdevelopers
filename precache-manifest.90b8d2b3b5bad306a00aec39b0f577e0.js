f491bf0a4c8cd78460008d70c782f2d951347e80
Mohit Agarwal <m.agarwal0805.com>
2020-04-20 14:15:40 +0530

    Updates

---
+++ precache-manifest.90b8d2b3b5bad306a00aec39b0f577e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0151bdc2b19c144d2259bcbbcec2ef5b",
+    "url": "/BBmsgdevelopers/index.html"
+  },
+  {
+    "revision": "cfbe22e80b3ecd8e9c4d",
+    "url": "/BBmsgdevelopers/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "2866616262f0841d1f90",
+    "url": "/BBmsgdevelopers/static/css/main.9d9ad9f6.chunk.css"
+  },
+  {
+    "revision": "cfbe22e80b3ecd8e9c4d",
+    "url": "/BBmsgdevelopers/static/js/2.145ad208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BBmsgdevelopers/static/js/2.145ad208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2866616262f0841d1f90",
+    "url": "/BBmsgdevelopers/static/js/main.7da5bf3d.chunk.js"
+  },
+  {
+    "revision": "0a33bdce86417fd5ffad",
+    "url": "/BBmsgdevelopers/static/js/runtime-main.1e1df631.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BBmsgdevelopers/static/media/logo.5d5d9eef.svg"
+  }
+]);
